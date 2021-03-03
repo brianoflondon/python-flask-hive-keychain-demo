e9feed13745ef85e8d50176a314e75f65992a741
Brian of London <bol@snnsite.com>
2021-03-03 13:45:53 +0200

    added loggin

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -3,6 +3,10 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
+# I add logging
+import logging
+
+
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
@@ -13,3 +17,6 @@ login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
 from flaskblog import routes
+
+logging.basicConfig(level=logging.INFO,
+                    format=f'%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s')
