caa35197560fb94af8808e3dd85aa91dce149888
Brian of London <bol@snnsite.com>
2021-03-04 11:22:53 +0200

    removed sqlalchemy warning

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -10,11 +10,16 @@ import logging
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# Get rid of annoying warning
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
+
+
+
 
 from flaskblog import routes
 
